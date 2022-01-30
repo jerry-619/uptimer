3f4b01b4c97b277c04095cc3d152505e9c384943
fardeen619 <60790859+fardeen619@users.noreply.github.com>
2022-01-30 16:44:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const { Presence } = require("discord.js");
 
 client.on("presenceUpdate", async (oldPresence, newPresence) => {
   if (!config.logger.enabled) return;
+    if (!newPresence.ID == "929716436107145226") return;
   try
     {
       if (config.logger.bots && !config.logger.humans) {
@@ -29,8 +30,7 @@ client.on("presenceUpdate", async (oldPresence, newPresence) => {
           if (!oldPresence || oldPresence.user.bot) return;
         }
         if (oldPresence.status == newPresence.status || oldPresence.status == "dnd" && newPresence.status == 'idle' || oldPresence.status == "idle" && newPresence.status == 'dnd' || oldPresence.status == "online" && newPresence.status == 'dnd' || oldPresence.status == "online" && newPresence.status == 'idle') return;
-if (newPresence.ID == "929716436107145226")
-{
+
         if (config.server_id)
         {
  
@@ -62,7 +62,6 @@ if (newPresence.ID == "929716436107145226")
                 });
             }
         }
-    }else return;
         } 
     catch (err)
     {
